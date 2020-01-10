f3d1817c9d5bddec646152981b9889b31d280562
unknown <calvinp003@gmail.com>
2020-01-10 09:53:23 -0800

    write code to pass project#delete test

--- lib/project.rb
+++ lib/project.rb
@@ -34,6 +34,10 @@ class Project
     DB.exec("UPDATE projects SET name = '#{@name}' WHERE id = #{@id}")
   end
 
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id}")
+  end
+
 
 
 end
