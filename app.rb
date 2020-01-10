54617e38efbd1b90ac0e1774d53e676be66e374e
unknown <calvinp003@gmail.com>
2020-01-10 11:51:31 -0800

    add some early routing and views

---
+++ app.rb
@@ -0,0 +1,20 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+
+get '/' do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post '/projects' do
+  project = Project.new({:name => params[:title], :id => nil})
+  redirct to ('/')
+end
